ed60c84400812b0c6a4b8540574ff7eff1ae73bf
Geordi Rugenbrink <G.rugenbrink@kpnmail.nl>
2019-01-21 13:55:35 +0100

    Created the project added a few assets like player movement and the camera following the player

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField]
+    private float movementSpeed = 6f;
+    [SerializeField]
+    private float diagonalMovementMultiplier = 0.75f;
+    private float currentMovementSpeed;
+
+
+    private void Move() {
+        transform.position += (new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * currentMovementSpeed) * Time.deltaTime;
+    }
+
+    private void Update() {
+        if(Mathf.Abs(Input.GetAxis("Horizontal")) >= 0.5f && Mathf.Abs(Input.GetAxis("Vertical")) >= 0.5f) {
+            currentMovementSpeed = movementSpeed * diagonalMovementMultiplier;
+        } else {
+            currentMovementSpeed = movementSpeed;
+        }
+
+        Move();
+    }
+}
